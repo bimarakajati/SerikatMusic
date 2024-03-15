9f61b9af6aba51c360f6e7deb6d84b20e3d36af6
bimarakajati <bimandugal@gmail.com>
2024-03-15 14:49:13 +0700

    Commented out code for auto cleaning messages

--- VenomX/plugins/misc/cleanmode.py
+++ VenomX/plugins/misc/cleanmode.py
@@ -70,24 +70,24 @@ async def auto_clean():
                     userstats[user_id].pop(0)
         except:
             continue
-        try:
-            for chat_id in clean:
-                if chat_id == config.LOGGER_ID:
-                    continue
-                for x in clean[chat_id]:
-                    if datetime.now() > x["timer_after"]:
-                        try:
-                            await app.delete_messages(
-                                chat_id, x["msg_id"]
-                            )
-                        except FloodWait as e:
-                            await asyncio.sleep(e.value)
-                        except:
-                            continue
-                    else:
-                        continue
-        except:
-            continue
+        # try:
+        #     for chat_id in clean:
+        #         if chat_id == config.LOGGER_ID:
+        #             continue
+        #         for x in clean[chat_id]:
+        #             if datetime.now() > x["timer_after"]:
+        #                 try:
+        #                     await app.delete_messages(
+        #                         chat_id, x["msg_id"]
+        #                     )
+        #                 except FloodWait as e:
+        #                     await asyncio.sleep(e.value)
+        #                 except:
+        #                     continue
+        #             else:
+        #                 continue
+        # except:
+        #     continue
         try:
             served_chats = await get_active_chats()
             for chat_id in served_chats:

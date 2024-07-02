0433806292f25d884fb6729126a3af4d6c17355f
bimarakajati <bimandugal@gmail.com>
2024-07-02 19:12:55 +0700

    Refactor bot start message formatting

--- VenomX/core/bot.py
+++ VenomX/core/bot.py
@@ -29,8 +29,8 @@ class Ayush(Client):
         try:
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                # text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}"
-                text=f'{self.mention} is running...'
+                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}"
+                # text=f'{self.mention} is running...'
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
